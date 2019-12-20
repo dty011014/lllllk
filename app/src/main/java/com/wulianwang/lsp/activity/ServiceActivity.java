285ef509f0b29933eaf5877dfab797ede25ca859
dty011014 <506624561@qq.com>
2019-12-20 09:34:15 +0800

    1.5  段天尧 叶皓明

--- app/src/main/java/com/wulianwang/lsp/activity/ServiceActivity.java
+++ app/src/main/java/com/wulianwang/lsp/activity/ServiceActivity.java
@@ -9,22 +9,30 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+
 import com.wulianwang.lsp.R;
 
-/**
- * 任务 1.5
- * 服务政策
- * 段天尧  叶皓明
- */
-
 public class ServiceActivity extends AppCompatActivity {
-        private ImageButton btn;
-        private TextView tv;
+    private ImageButton btn;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+        btn=findViewById(R.id.btn);
+        tv=findViewById(R.id.tv_3);
+        tv.setMovementMethod(ScrollingMovementMethod.getInstance());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent =new Intent(ServiceActivity.this,MainActivity.class);
+                //startActivity(intent);
+                onBackPressed();
 
+
+            }
+        });
     }
 }
+
